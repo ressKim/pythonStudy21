f87741906b10c005d2c11f02987f97d52e570610
jun <sh21dlat@gmail.com>
2021-01-09 01:49:17 +0900

    list update 222

--- listStudy.py
+++ listStudy.py
@@ -1,18 +1,50 @@
-
-subway = [10, 20, 30]
-print(subway)
-
-subway = ["1번", "2번"]
-print(subway.index("2번"))
-
-subway.append("4번")
-print(subway)
-
-subway.insert(1, "추가")
-print(subway)
-
+#
+# subway = [10, 20, 30]
+# print(subway)
+#
+# subway = ["1번", "2번"]
+# print(subway.index("2번"))
+#
+# subway.append("4번")
+# print(subway)
+#
+# subway.insert(1, "추가")
+# print(subway)
+#
+# # print(subway)
+#
+# print(subway.count("2번"))
+# subway.append("에베베")
+# print(subway)
+#
+# print(subway.pop())
+# print(subway)
+# print(subway.pop())
 # print(subway)
 
-print(subway.count("2번"))
-subway.append("에베베")
-print(subway)
+num_list = [3, 4, 2, 1, 5]
+num_list.sort()
+print(num_list)
+num_list.reverse()
+print(num_list)
+
+num_list.clear()
+
+print(num_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
